d30c6c9d1e527d3f0c7a6602b02c34b60dd276b5
Oleg <exampl@gmail.com>
2021-08-10 17:30:14 +0300

    new lesson

--- js/script.js
+++ js/script.js
@@ -6,25 +6,61 @@
 // ЗАПОМНИТЬ вся инфа от пользователя приходит ВСЕГДА в виде типа строка (String)
 
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
 
-const personalMovieDB = {
-        count: numberOfFilms,
-        movies: {},
-        action: {},
-        genres: [],
-        privat: false
-};
+// const personalMovieDB = {
+//         count: numberOfFilms,
+//         movies: {},
+//         action: {},
+//         genres: [],
+//         privat: false
+// };
 
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = prompt("На сколько оцените его?", "");
+// const a = prompt("Один из последних просмотренных фильмов?", ""),
+//       b = prompt("На сколько оцените его?", ""),
+//       c = prompt("Один из последних просмотренных фильмов?", ""),
+//       d = prompt("На сколько оцените его?", "");
 
-      
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+// const num = 55;
+
+// if (num < 49) {
+//     console.log('num menshe 50');
+// } else if (num > 100) {
+//     console.log('num bolshe 100');
+// } else {
+//     console.log(num);
+// }
+
+// Тернарный оператор
+//(num == 53) ? console.log('Yes') : console.log('Nooo');
+
+// const num = 40;
+
+// switch (num) {
+//     case 49:  
+//         console.log('num = 49');
+//         break;
+//     case 100:
+//         console.log('num = 100');
+//         break;
+//     case 50:
+//         console.log('num = 50');
+//         break;
+//     default:
+//         console.log('default');
+//         break;        
+// }
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 
