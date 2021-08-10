8c8165ed99cbe1b9dae1f7facd4df7c311a990dd
Oleg <exampl@gmail.com>
2021-08-10 16:08:39 +0300

    update all files

--- js/script.js
+++ js/script.js
@@ -5,19 +5,7 @@
 // console.log(answer + 5);
 // ЗАПОМНИТЬ вся инфа от пользователя приходит ВСЕГДА в виде типа строка (String)
 
-//const answers = [];
-// answers[0] = prompt("Как тебя зовут");
-// answers[1] = prompt("Как тебя Фамилия?");
-// answers[2] = prompt("Сколько тебе лет?");
 
-// const category = 'toysssssss';
-// console.log(`https://someurl.com/${category}/5`);
 
-//console.log((2+2)*2 != '8');
-// console.log(++incr);
-// console.log(--decr);
-
-// const isChecked = true, isClose = true;  
-// console.log(!isChecked || isClose);
 
 
