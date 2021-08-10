b1d00e543efe8d299bb03474ac7a3546ac889dbe
Oleg <exampl@gmail.com>
2021-08-10 16:49:52 +0300

    Правка Script file

--- js/script.js
+++ js/script.js
@@ -6,6 +6,25 @@
 // ЗАПОМНИТЬ вся инфа от пользователя приходит ВСЕГДА в виде типа строка (String)
 
 
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        action: {},
+        genres: [],
+        privat: false
+};
 
 
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = prompt("На сколько оцените его?", "");
+
+      
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
 
