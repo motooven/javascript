4a7c2f863a4e0e39dc41bc3ef9af495b17bcc1bb
Oleg <exampl@gmail.com>
2021-08-11 16:22:15 +0300

    11.08.21 || 16:22

--- js/script.js
+++ js/script.js
@@ -168,3 +168,33 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Новая тема 15:25
 
+
+
+// const str = 'teSt';
+
+// console.log('Большие буквы:', str.toUpperCase());
+// console.log('Маленькие буквы:', str.toLocaleLowerCase());
+// console.log('Default any:', str);
+
+
+// const fruit = "Some fruit";
+
+// console.log(fruit.indexOf("fruit"));
+
+// const logg = "Hello world";
+
+// console.log(logg.slice(6, 11));
+
+// console.log(logg.substring(6, 11)); //тоже самое что и мтод slice
+
+// console.log(logg.substr(6, 2));
+
+// const num = 12.2;
+// console.log(Math.round(num));
+
+
+// const test = "12.2px";
+// console.log(parseInt(test)); 
+// console.log(parseFloat(test));
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
